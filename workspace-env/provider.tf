fc8c50794f0f7224678752ec61456702e69ef067
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-29 15:20:08 -0700

    ALL

---
+++ workspace-env/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.64.0"
+    }
+  }
+ backend "s3" {
+    bucket = "daws78s-dev4"
+    key    = "multi-workspace"
+    region = "us-east-1"
+    dynamodb_table="daws78s-dev4"
+  }
+}
+
+provider "aws" {
+    region="us-east-1"
+  # Configuration options
+}
